066f882a73182b7aa4a5e35568eaba0a82f5681b
harvey298 <marco77@live.co.uk>
2020-08-03 00:46:14 +0100

    Update Reference.java
    changed the mod id

--- src/main/java/com/harvey298/PATD/util/Reference.java
+++ src/main/java/com/harvey298/PATD/util/Reference.java
@@ -1,7 +1,7 @@
 package com.harvey298.PATD.util;
 
 public class Reference {
-    public static final String MOD_ID = "pm";
+    public static final String MOD_ID = "pdathreat";
     public static final String NAME = "Automated Threat Detection Mod";
     public static final String VERSION = "1.0";
     public static final String ACCEPTED_VERISONS = ("1.12.2");
