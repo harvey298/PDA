0ae0fa29bac9380c64bf0e3572d5e2fece024602
harvey298 <marco77@live.co.uk>
2020-08-04 20:32:37 +0100

    Update Main.java
    fixed imports

--- src/main/java/com/harvey298/PATD/Main.java
+++ src/main/java/com/harvey298/PATD/Main.java
@@ -4,6 +4,7 @@ import com.harvey298.PATD.proxy.CommonProxy;
 import com.harvey298.PATD.util.Reference;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
